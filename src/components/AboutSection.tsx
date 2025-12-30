e4ec929ce45de7985debcd64cd47f946508a7a4d
Emoni Samuel <emonisamue54@gmail.com>
2025-12-30 21:10:40 +0300

    profile image path fix

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import { CheckCircle2, Code, Palette, Target, Heart, Search, Lightbulb, Rocket, CheckCheck, Award, TrendingUp, Users, Zap, Star, Briefcase, Coffee, Globe, Download, Database, MessageCircle, BarChart, DollarSign, Video, Computer, Wrench } from "lucide-react";
 
 export default function AboutSection() {
-  const profileImageUrl = "public/assets/sam.jpg";
+  const profileImageUrl = "/assets/sam.jpg";
   const [currentSkillIndex, setCurrentSkillIndex] = useState(0);
   const [activeTab, setActiveTab] = useState("journey");
 
